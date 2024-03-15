c0428ef29292f179188693e30b37829a9a493941
LauCaz <74980870+LauCaz@users.noreply.github.com>
2024-03-15 14:37:08 +0100

    Uploaded relevant files for estimating the models

---
+++ BiogemeGRDM.py
@@ -0,0 +1,20 @@
+import biogeme.biogeme as bio
+from biogeme import models
+from biogeme.expressions import Beta
+
+#%%
+
+from swissmetro_data import (
+    database,
+    CHOICE,
+    SM_AV,
+    CAR_AV_SP,
+    TRAIN_AV_SP,
+    TRAIN_TT_SCALED,
+    TRAIN_COST_SCALED,
+    SM_TT_SCALED,
+    SM_COST_SCALED,
+    CAR_TT_SCALED,
+    CAR_CO_SCALED,
+)
+#%%
